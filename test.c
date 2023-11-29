c351aca96276498a6f3448e8b4f2991e11a9d1e7
PewPewJelly <137582221+PewPewJelly@users.noreply.github.com>
2023-11-29 12:59:10 +0900

    dd

--- test.c
+++ test.c
@@ -8,21 +8,29 @@ void asd(char input,char name){
 }
 
 main(){
-	int i;
+	int i,n;
     char input[100];
     char name[100];
+	//번호
+	scanf("%d-1",&n);
+
+	printf("%d",n);
+
+	//코드 입력
     scanf("%s",&input);
+	strcpy(m[n][0],input);
+	//이름
     scanf("%s",&name);
-    
-	strcpy(m[0][0],input);
-	strcpy(m[0][1],name);
+	strcpy(m[n][1],name);
 	
 	printf("%s %s\n",input,name);
 
+	for(i=0;i<10;i+=1){
+	printf("%s ",m[i][0]);
 
-	printf("%s ",m[0][0]);
+	printf("%s \n",m[i][1]);
+	}
 
-	printf("%s",m[0][1]);
 
 	
 	
